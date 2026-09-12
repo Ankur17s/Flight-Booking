7be75e5535457649880b14f0907d0d98d09b97d1
Ankur Srivastav <ankursrivastav44608@gmail.com>
2026-09-12 20:34:35 +0530

    added created city api

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     InfoController: require('./info-controller'),
-    AirplanceController: require('./airplane-controller')
+    AirplanceController: require('./airplane-controller'),
+    CityController: require('./city-controller')
 }
---
+++ src/repositories/city-repository.js
@@ -0,0 +1,10 @@
+const CRUDRepository = require("./crud-repository");
+const { City } = require('../models')
+
+class CityRepository extends CRUDRepository {
+    constructor() {
+        super(City)
+    }
+}
+
+module.exports = CityRepository
