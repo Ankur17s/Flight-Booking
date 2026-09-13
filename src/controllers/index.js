fdd776ba4766cf4a6b0e4d5763fdca201b4842cc
Ankur Srivastav <ankursrivastav44608@gmail.com>
2026-09-13 20:20:29 +0530

    added airport resource crud

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     InfoController: require('./info-controller'),
     AirplanceController: require('./airplane-controller'),
-    CityController: require('./city-controller')
+    CityController: require('./city-controller'),
+    AirportController: require('./airport-controller')
 }
--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     AirplaneMiddlewares: require('./airplane-middleware'),
-    CityMiddleWares: require('./city-middleware')
+    CityMiddleWares: require('./city-middleware'),
+    AirportMiddlewares: require('./airport-middleware')
 }
